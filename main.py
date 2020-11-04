01509afe7011e546fe330d2214b81a1f6f8784e1
Krzysztof <kbzowski@agh.edu.pl>
2020-11-04 17:18:17 +0100

    Pobierane danych dla wszystkich z dyscypliny

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+import json
 import pprint
 import re
 import os
@@ -12,7 +13,9 @@ import requests
 import csv
 from collections import OrderedDict
 
+from disciplines import Discipline
 from faculties import Faculty
+
 # user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
 options = Options()
 options.headless = False
@@ -44,17 +47,24 @@ def is_author_alive(author):
     print('{0:10} {1:60s} PRACOWNIK'.format(author_id, author_name))
     return True
 
-def get_authors_id_by_faculty(faculty, filter_by_department=None):
-    faculty_id = faculty.value
-    browser.get('https://www.bpp.agh.edu.pl/?wydz={0}'.format(faculty_id))
-    faculty_name = Select(browser.find_element_by_id('wydz')).first_selected_option.text
-    print(colorama.Style.BRIGHT + 'Fetching staff from: {0}\n'.format(faculty_name))
 
+def get_authors_id_by_faculty(faculty=None, filter_by_department=None):
+    if faculty is not None:
+        faculty_id = faculty.value
+        browser.get('https://www.bpp.agh.edu.pl/?wydz={0}'.format(faculty_id))
+        faculty_name = Select(browser.find_element_by_id('wydz')).first_selected_option.text
+        print(colorama.Style.BRIGHT + 'Fetching staff from: {0}\n'.format(faculty_name))
 
     authors_links = []
     letters = ascii_uppercase + "ĆŚŹŻŁ"
     for char in letters:
-        browser.get("https://www.bpp.agh.edu.pl/indeks/?wydz={0}&odR=0&doR=0&poz={1}".format(faculty_id, char.upper()))
+        print("Fetching all authors starting with: {}".format(char))
+        if faculty is not None:
+            browser.get(
+                "https://www.bpp.agh.edu.pl/indeks/?wydz={0}&odR=0&doR=0&poz={1}".format(faculty_id, char.upper()))
+        else:
+            browser.get(
+                "https://www.bpp.agh.edu.pl/indeks/?poz={0}".format(char.upper()))
 
         table = browser.find_element_by_id('laut')
         authors = table.find_elements_by_tag_name("tr")
@@ -68,7 +78,7 @@ def get_authors_id_by_faculty(faculty, filter_by_department=None):
                     continue
 
                 author_id = re.search('([0-9]+)', author_url).groups(1)[0]
-                authors_links.append((author_id, author_department))
+                authors_links.append(author_id)
 
     return list(set(authors_links))
 
@@ -85,12 +95,13 @@ def get_pages_navs_buttons():
     pages = browser.find_elements_by_tag_name("input")
     return [p for p in pages if 'pozycje' in p.get_attribute("title")]
 
+
 def add_to_evaluation(evaluation_data, new_item, author_name, paperName, errors):
     discipline = new_item['nazwa_dyscypliny']
     if new_item['sloty_p_u_'] is None or new_item['sloty_u_'] is None:
         errors.append({
             'authorName': author_name,
-            'paperName': paperName,
+            'paperName': paperName.replace('\n', ' ').replace('\r', ''),
             'paperId': new_item['id_publ'],
             'summPoints': new_item['wzor_p_c']
         })
@@ -114,18 +125,21 @@ def save_global_evaluation_to_csv(global_evaluation):
             headers['{}_p'.format(discipline)] = 0
             headers['{}_s'.format(discipline)] = 0
 
-
-
-    with open('evaluation.csv', mode='w', newline='', encoding='utf-8') as csv_file:
-        filelds = ['imie', 'nazwisko', 'department'] + list(headers)
-        writer = csv.DictWriter(csv_file, fieldnames=filelds)
+    with open('evaluation.csv', mode='w', newline='', encoding='utf-8', buffering=1) as csv_file:
+        filelds = ['imie', 'nazwisko', 'department', 'faculty', "disc1", "disc1_percent", "disc2", "disc2_percent" ] + list(headers)
+        writer = csv.DictWriter(csv_file, fieldnames=filelds, extrasaction='ignore')
         writer.writeheader()
 
         for person in global_evaluation:
             data_to_write = {
-                "imie": person["name"].split(" ")[0],
-                "nazwisko": person["name"].split(" ")[1],
-                "department": person["department"]
+                "imie": person['info']["imie"],
+                "nazwisko": person['info']["nazwisko"],
+                "department": person['info']["department"],
+                "faculty": person['info']["faculty"],
+                "disc1": person['info']["disc1"],
+                "disc1_percent": person['info']["disc1_percent"],
+                "disc2": person['info']["disc2"],
+                "disc2_percent": person['info']["disc2_percent"],
             }
             for discipline in person['summary'].keys():
                 data_to_write['{}_p'.format(discipline)] = person['summary'][discipline]['points']
@@ -143,20 +157,108 @@ def save_errors_to_csv(errors):
             writer.writerow(error)
 
 
+def get_author_info(author_id):
+    browser.get("https://bpp.agh.edu.pl/autor/{}".format(author_id))
+    author_name_with_title = browser.find_element_by_css_selector('h2').text.split(',')
+    author_name = author_name_with_title[0]
+
+    info = {
+        "imie": ' '.join(author_name.split(' ')[0:-1]),
+        "nazwisko": author_name.split(" ")[-1],
+        "degree": author_name_with_title[1].strip() if len(author_name_with_title) > 1 else None,
+    }
+
+    try:
+        browser.find_element_by_partial_link_text('System Informacyjny AGH')
+        info['alive'] = True
+    except:
+        info['alive'] = False
+
+
+    inf_author_element = browser.find_elements_by_xpath("//*[@id='infAutor']")[0].text
+
+    splitted_info = inf_author_element.split('\n')
+    if splitted_info[1].startswith('poprzednio'):
+        faculty = splitted_info[2] if len(inf_author_element) > 1 else None
+        department = splitted_info[3] if len(inf_author_element) > 2 else None
+    else:
+        faculty = splitted_info[1] if len(inf_author_element) > 1 else None
+        department = splitted_info[2] if len(inf_author_element) > 2 else None
+
+    disc1 = re.search('\[dyscyplina wiodąca](.*)', inf_author_element)
+    disc2 = re.search('\[dyscyplina dodatkowa](.*)', inf_author_element)
+    disc1_percent = 0
+    disc2_percent = 0
+
+    disc1_text_only = ''
+    disc2_text_only = ''
+
+    if disc1:
+        disc1_text_only = disc1.groups(1)[0].strip() if disc1 is not None else ''
+        disc1_percent = 1
+
+    if disc2:
+        disc2_text_only = re.sub(r'\([0-9][0-9]%\)', '', disc2.groups(1)[0].strip()).strip()
+        disc2_percent = float(re.search('([0-9][0-9])', disc2.groups(1)[0].strip()).groups(1)[0]) / 100
+        disc1_percent = 1 - disc2_percent
+
+    info['id'] = author_id
+    info['disc1'] = disc1_text_only
+    info['disc2'] = disc2_text_only
+    info['disc1_percent'] = disc1_percent
+    info['disc2_percent'] = disc2_percent
+    info['faculty'] = faculty
+    info['department'] = department
+    return info
+
+
+def get_authors_by_discipline(discipline, min_percent = 0):
+    authors_links = get_authors_id_by_faculty()
+
+    filtered_list = []
+    all_authors = []
+
+    for author_id in authors_links:
+        info = get_author_info(author_id)
+        if info['alive']:
+            all_authors.append(info)
+            if info['disc1'] == discipline.value and info['disc1_percent'] > min_percent:
+                filtered_list.append(author_id)
+            if info['disc2'] == discipline.value and info['disc2_percent'] > min_percent:
+                filtered_list.append(author_id)
+
+    json_dump = json.dumps(all_authors, ensure_ascii=False)
+    file = open('authors.json', 'w', encoding='utf8')
+    file.write(json_dump)
+    file.close()
+
+
+    return list(set(filtered_list))
+
+    # with open('agh.csv', mode='w', newline='', encoding='utf-8', buffering=1) as csv_file:
+    #     filelds = ['id', 'imie', 'nazwisko', 'faculty', 'department', 'disc1', 'disc1_percent', 'disc2', 'disc2_percent']
+    #     writer = csv.DictWriter(csv_file, fieldnames=filelds, extrasaction='ignore')
+    #     writer.writeheader()
+    #
+    #     for author_id in authors_links:
+    #         info = get_author_info(author_id)
+    #         if info['alive']:
+    #             writer.writerow(info)
+
+
+
 def run(authors, from_year, to_year):
     file = open('result.txt', 'w', encoding='utf8')
-    department_points = []
+    # department_points = []
     global_evaluation = []
     errors = []
 
     for author in authors:
-        author_id = author[0]  # id
-        if isinstance(author_id, int):
-            author_id = str(author_id)
+        author_id = author # id
+        info = get_author_info(author_id)
 
-        department_name = author[1]  # department
         author_evaluation = {}
-        sum_points = 0
+        # sum_points = 0
         pubs_list_filtered_url = 'https://bpp.agh.edu.pl/autor/?idA={0}&idform=1&afi=1&f1Search=1&fodR={1}&fdoR={2}&fagTP=0&fagPM=on'.format(
             author_id, from_year, to_year)
         browser.get(pubs_list_filtered_url)
@@ -191,44 +293,43 @@ def run(authors, from_year, to_year):
                         except:
                             pass
 
-
         for paper in papers_ids:
             paperId = paper['id']
             paperName = paper['name']
-            browser.get(
-                'https://bpp.agh.edu.pl/htmle.php?file=publikacja-pktm-iflf.html&id_publ={0}&id_autor={1}'.format(
-                    paperId, author_id))
-            points_string = browser.find_element_by_class_name('ocena-pktm').text
-            points_string_list = points_string.split('\n')
-            last_evaluation_points = points_string_list[0]
-            extracted_points = re.search('.*?: (([0-9]*[.])?[0-9]+)', last_evaluation_points).groups(1)[0]
-            points = float(extracted_points)
-            sum_points += points
+            # browser.get(
+            #     'https://bpp.agh.edu.pl/htmle.php?file=publikacja-pktm-iflf.html&id_publ={0}&id_autor={1}'.format(
+            #         paperId, author_id))
+            # points_string = browser.find_element_by_class_name('ocena-pktm').text
+            # points_string_list = points_string.split('\n')
+            # last_evaluation_points = points_string_list[0]
+            # extracted_points = re.search('.*?: (([0-9]*[.])?[0-9]+)', last_evaluation_points).groups(1)[0]
+            # points = float(extracted_points)
+            # sum_points += points
 
-            #evaluation
+            # evaluation
             paper_eval = get_publication_evaluation(author_id, paperId)
             if len(paper_eval) > 0:
-                add_to_evaluation(author_evaluation, paper_eval[0], author_name, paperName, errors)         # 0 if for evaluation 2021... probably
+                add_to_evaluation(author_evaluation, paper_eval[0], author_name, paperName,
+                                  errors)  # 0 if for evaluation 2021... probably
 
         global_evaluation.append({
-            "name": author_name,
-            "department": department_name,
+            "info": info,
             "summary": author_evaluation
         })
 
-        log = '{0:10s} {1:60s}: {2}'.format(author_id, author_name, sum_points)
-        print(log)
-        log = '{0:10s} {1}\t{2}\t{3}'.format(author_id, author_name, department_name, sum_points)
-        file.write(log + "\n")
-        department_points.append(sum_points)
+        # log = '{0:10s} {1:60s}: {2}'.format(author_id, author_name, sum_points)
+        # print(log)
+        # log = '{0:10s} {1}\t{2}\t{3}'.format(author_id, author_name, department_name, sum_points)
+        # file.write(log + "\n")
+        # department_points.append(sum_points)
 
     save_global_evaluation_to_csv(global_evaluation)
     save_errors_to_csv(errors)
     browser.quit()
     file.close()
-    median = statistics.median(department_points)
-    mean = statistics.mean(department_points)
-    print('\n\nMedian for specific criteria: {0}\nAverage points: {1}'.format(median, mean))
+    # median = statistics.median(department_points)
+    # mean = statistics.mean(department_points)
+    # print('\n\nMedian for specific criteria: {0}\nAverage points: {1}'.format(median, mean))
 
 
 if __name__ == "__main__":
@@ -240,9 +341,13 @@ if __name__ == "__main__":
     # DEPARTMENT = 'WIMiIP-kism'
     ###############################################
 
-
-    authors_ids = get_authors_id_by_faculty(FACULTY, DEPARTMENT)
-    authors_ids = [a for a in authors_ids if is_author_alive(a)]
-    #authors_ids = [(4838, ''), (2776, ''), (2773, ''), (6330, ''), (2775, ''), (2770, ''), (5138, ''), (21023, ''), (7173, ''), (26298, ''), (3942, ''), (6353, ''), (5929, ''), (4650, ''), (4667, ''), (3655, ''), (3556, ''), (4651, ''), (7225, ''), (5861, ''), (5063, ''), (6991, ''), (5973, ''), (7213, ''), (17069, ''), (31825, ''), (5828, ''), (18663, ''), (4844, ''), (33863, ''), (35207, ''), (5010, ''), (17548, ''), (5854, ''), (6357, ''), (5008, ''), (4174, ''), (5601, ''), (4843, ''), (7100, ''), (6468, ''), (2767, ''), (6152, ''), (12206, ''), (6855, ''), (20770, ''), (4360, ''), (5783, ''), (9040, '')]
+    # authors_ids = get_authors_id_by_faculty(FACULTY, DEPARTMENT)
+    # authors_ids = [a for a in authors_ids if is_author_alive(a)]
+    # authors_ids = [(4838, ''), (2776, ''), (2773, ''), (6330, ''), (2775, ''), (2770, ''), (5138, ''), (21023, ''), (7173, ''), (26298, ''), (3942, ''), (6353, ''), (5929, ''), (4650, ''), (4667, ''), (3655, ''), (3556, ''), (4651, ''), (7225, ''), (5861, ''), (5063, ''), (6991, ''), (5973, ''), (7213, ''), (17069, ''), (31825, ''), (5828, ''), (18663, ''), (4844, ''), (33863, ''), (35207, ''), (5010, ''), (17548, ''), (5854, ''), (6357, ''), (5008, ''), (4174, ''), (5601, ''), (4843, ''), (7100, ''), (6468, ''), (2767, ''), (6152, ''), (12206, ''), (6855, ''), (20770, ''), (4360, ''), (5783, ''), (9040, '')]
     # authors_ids = [('05344', 'WIMiIP-kism')]       # For specific author
+    # run(authors_ids, FROM_YEAR, TO_YEAR)
+
+    authors_ids = get_authors_by_discipline(Discipline.INZYNIERIA_MATERIALOWA)
+    # print(authors_ids)
+    # authors_ids = ['05063']
     run(authors_ids, FROM_YEAR, TO_YEAR)
